2a0ba1de030a0acd4de24276f0fcfe435db26737
louvre11 <133773207+louvre11@users.noreply.github.com>
2023-05-16 15:32:35 +0000

    Update finalproj.py

--- finalproj.py
+++ finalproj.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
-def load_model():
+def load_weights():
   model=tf.keras.models.load_weights('model1.h5')
   return model
 model=load_weights()
