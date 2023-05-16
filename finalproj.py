32acd63fb61e0b9f2ec5aa303164527c669c6129
louvre11 <133773207+louvre11@users.noreply.github.com>
2023-05-16 15:28:47 +0000

    Update finalproj.py

--- finalproj.py
+++ finalproj.py
@@ -3,8 +3,7 @@ import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
 def load_model():
- 
-  model=model.load_weights('model1.h5')
+ model=tf.keras.models.load_weights('model1.h5')
   return model
 model=load_model()
 st.write("""
