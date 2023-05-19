72f5942e82c804df1fe3756041b21282fde07296
louvre11 <133773207+louvre11@users.noreply.github.com>
2023-05-19 12:54:41 +0000

    Update finalproj.py

--- finalproj.py
+++ finalproj.py
@@ -15,9 +15,9 @@ with st.sidebar:
     "Select the image that you would like to display",
     ('Lion', 'Cheetah'))
     image1 = Image.open('Lion.png')
-      if chart == 'Lion': 
+     if chart == 'Lion': 
         st.image(image1)
-      if chart == 'Cheetah': 
+     if chart == 'Cheetah': 
         st.image(image1)
 
 
