d5bd6cc2d6e96743a36b0b385859240a28bba5e8
louvre11 <133773207+louvre11@users.noreply.github.com>
2023-05-19 14:19:18 +0000

    Update finalproj.py

--- finalproj.py
+++ finalproj.py
@@ -20,7 +20,7 @@ st.markdown(
 # The side bar that contains radio buttons for selection of charts
 with st.sidebar:
     st.header('Select the image that you would like to display')
-    chart = st.radio("Sample Image", ('Lion', 'Cheetah'))
+    chart = st.radio("Sample Image", ('Lion', 'Cheetah','Conclusion'))
     
     if chart == 'Lion': 
       image1 = Image.open('Lion.jpg')
@@ -28,8 +28,16 @@ with st.sidebar:
     if chart == 'Cheetah': 
       image1 = Image.open('Cheetah.jpg')
       st.image(image1)
+    if chart == 'Conclusion':
+      st.title("In this task, I successfully deployed a deep learning model in the cloud. Specifically, I utilized a Convolutional Neural Network (CNN) 
+               and selected the Lion Vs. Cheetah dataset. To accomplish the deployment, I needed a GitHub account, which I used to sign in to a Streamlit website. 
+               It can be denoted that to successfully deploy the model, necessary libraries should be installed such as the tensorflow, streamlit, numpy, etc.
+               During the activity, I encountered an error stating "no module named TensorFlow," even though I had already installed it. Upon researching the issue, 
+               I discovered that the error was due to the file type I used. Initially, I used the H5 file format and attempted to load the model using `.load_weights()`. 
+               After realizing this was the source of the error, I switched to using the Hdf5 file format and saved the model using the command `model.save()`. 
+               While any file format can be used to deploy the model, I recommend using the `.save()` method instead of just saving the weights.")
  
-    my_component(greeting="hello")
+    
 
 st.title("Lion or Cheetah Classifier")
 st.info("An image classifying project that differentiates between two very similar-looking wild cats: Cheetahs and Lion using Python and TensorFlow")
