438c2f0a09e39b1e7a22244eb79f0bf22356d8e2
Lou Gesite <133773207+louvreeee@users.noreply.github.com>
2023-10-15 22:33:54 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -67,12 +67,12 @@ file=st.file_uploader("Choose photo from computer, must be a lion or cheetah",ty
 import cv2
 from PIL import Image,ImageOps
 import numpy as np
-def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+def import_and_predict(image_data, model):
+    size = (64, 64)
+    image = image_data.resize(size)  # Resize without using Image.ANTIALIAS
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis, ...]
+    prediction = model.predict(img_reshape)
     return prediction
 if file is None:
     st.text("Please upload an image file")
