02663acd9959a655b77da6267e487dfb8fc567a7
Jaymiiee <133840068+Jaymiiee@users.noreply.github.com>
2023-10-16 10:15:37 +0800

    User Feedback added Feature
    Signed-off-by: Jaymiiee <133840068+Jaymiiee@users.noreply.github.com>

--- DevOps-3.py
+++ DevOps-3.py
@@ -112,3 +112,44 @@ else:
     class_names=['Lion','Cheetah']
     string="The picture shown above is a  "+class_names[np.argmax(prediction)]
     st.success(string)
+
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data, model):
+    size = (64, 64)
+    image = image_data.resize(size)  # Resize without using Image.ANTIALIAS
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis, ...]
+    prediction = model.predict(img_reshape)
+    return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    class_names=['Lion','Cheetah']
+    string="The picture shown above is a  "+class_names[np.argmax(prediction)]
+    st.success(string)
+
+
+    # User feedback section
+    st.header("User Feedback")
+    user_feedback = st.checkbox("Was this classification correct?")
+
+    if st.button("Submit Feedback"):
+        collect_user_feedback(file.name, class_names[np.argmax(prediction)], user_feedback)
+        st.success("Feedback submitted successfully!")
+
+    # Display statistics or insights based on user interactions and collected feedback
+    if st.session_state.feedback_data_exists:
+        feedback_data = pd.read_csv('user_feedback.csv')
+
+        total_feedback = len(feedback_data)
+        correct_feedback = len(feedback_data[feedback_data['UserFeedback']])
+
+        st.subheader("Feedback Statistics")
+        st.write(f"Total Feedback Submitted: {total_feedback}")
+        st.write(f"Correct Feedback: {correct_feedback}")
+        st.write(f"Feedback Accuracy: {correct_feedback / total_feedback:.2%}")
