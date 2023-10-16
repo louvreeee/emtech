5fd77d17471d31ebbf76b8ace3a4f7b23c5d1850
Jaymiiee <133840068+Jaymiiee@users.noreply.github.com>
2023-10-16 10:05:39 +0800

    DevOps Updated User Feedback Function
    Signed-off-by: Jaymiiee <133840068+Jaymiiee@users.noreply.github.com>

--- DevOps-3.py
+++ DevOps-3.py
@@ -95,8 +95,6 @@ st.info("""To view and display a sample image of a cheetah or lion, please selec
 file=st.file_uploader("Choose photo from computer, must be a lion or cheetah",type=["jpg","png"])
 
 
-import cv2
-from PIL import Image, ImageOps
 import numpy as np
 def import_and_predict(image_data, model):
     size = (64, 64)
@@ -114,6 +112,3 @@ else:
     class_names=['Lion','Cheetah']
     string="The picture shown above is a  "+class_names[np.argmax(prediction)]
     st.success(string)
-
-
-   
